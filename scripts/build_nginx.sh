92085c96fd51140b1091b24606722ec44b94f82b
François Pradel <francois.pradel@gmail.com>
2019-02-23 23:29:56 +0100

    Update Nginx, PCRE, OpenSSL & add support for heroku-18 stack

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,10 +9,10 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.11.8}
-PCRE_VERSION=${PCRE_VERSION-8.39}
+NGINX_VERSION=${NGINX_VERSION-1.14.2}
+PCRE_VERSION=${PCRE_VERSION-8.42}
 ZLIB_VERSION=${ZLIB_VERSION-1.2.11}
-OPENSSL_VERSION=${OPENSSL_VERSION-1.0.2j}
+OPENSSL_VERSION=${OPENSSL_VERSION-1.1.1a}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
